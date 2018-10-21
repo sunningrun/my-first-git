dd445c8255e69ee2772302ea43d76d6dd82250fc
jingxiang <dianluxitong@126.com>
2018-10-21 09:18:32 +0800

    start
    Signed-off-by: jingxiang <dianluxitong@126.com>

---
+++ cn-study-web/src/main/java/cn/study/CnStudyWebApplication.java
@@ -0,0 +1,12 @@
+package cn.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnStudyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnStudyWebApplication.class, args);
+	}
+}
